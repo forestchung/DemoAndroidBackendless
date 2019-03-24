ad402f899e76aea5830d3b147dd050bdedbec228
user <ykchung@cityu.edu.hk>
2019-03-24 15:01:31 +0800

    Initial commit

---
+++ src/main/java/com/mbass/examples/Defaults.java
@@ -0,0 +1,11 @@
+package com.mbass.examples;
+
+public class Defaults
+{
+  
+public static final String APPLICATION_ID = "5FE8A9D4-EB49-73BE-FF64-68A9E90C2100";
+public static final String API_KEY = "646E195B-FCE0-39FA-FF4E-A88AC3D58400";
+public static final String SERVER_URL = "https://api.backendless.com";
+    
+}
+                                            
